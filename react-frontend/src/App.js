63a6ba743f0fd37c7ef1cfe2a647fa8a22573a0e
undetected_landmine <kumar.pandey@magicedtech.com>
2023-04-03 17:20:40 +0530

    Initial Build

---
+++ react-frontend/src/App.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './App.css';
+import ListEmployeeComponent from './components/ListEmployeeComponent'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <ListEmployeeComponent />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-frontend/src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,35 @@
+
+import React, { useState, useEffect } from 'react';
+
+const ListEmployeeComponent = () => {
+  const [employees, setEmployees] = useState([]);
+
+  return (
+    <div>
+      <h2 className="text-center">Employees List</h2>
+      <div className="row">
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th> Employee First Name</th>
+              <th> Employee Last Name</th>
+              <th> Employee Email Id</th>
+              <th> Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {employees.map((employee) => (
+              <tr key={employee.id}>
+                <td> {employee.firstName} </td>
+                <td> {employee.lastName}</td>
+                <td> {employee.emailId}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ListEmployeeComponent;
